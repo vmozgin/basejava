e167be829f290a81a030c2419e4d4b12739151f9
vmozgin <d0mASDzxc>
2021-03-08 23:10:05 +0300

    lesson1, HW1, fix1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,11 +4,13 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int storageSize = 0;
 
     void clear() {
         for (int i = 0; i < storage.length - 1; i++) {
             storage[i] = null;
         }
+        storageSize = 0;
     }
 
     void save(Resume r) {
@@ -18,10 +20,10 @@ public class ArrayStorage {
                 break;
             }
         }
+        storageSize++;
     }
 
     Resume get(String uuid) {
-
         for (Resume resume : storage) {
             if (resume != null) {
                 if (resume.uuid.equals(uuid)) {
@@ -33,42 +35,32 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < storage.length - 1; i++) {
-            if (storage[i] != null) {
-                if (storage[i].uuid.equals(uuid)) {
-                    storage[i] = null;
-                    break;
-                }
+        for (int i = 0; i < storageSize; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                break;
+
             }
         }
-        for (int i = 0; i < storage.length - 1; i++) {
-            if (storage[i] == null) {
-                storage[i] = storage[i + 1];
-                storage[i + 1] = null;
-            }
+        for (int i = 0; i < storageSize; i++) {
+            storage[i] = storage[i + 1];
+            storage[i + 1] = null;
         }
+        storageSize--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] resumeWithoutNulls = new Resume[size()];
-        for (int i = 0; i < storage.length - 1; i++) {
-            if (storage[i] != null) {
-                resumeWithoutNulls[i] = storage[i];
-            }
+        Resume[] resumeWithoutNulls = new Resume[storageSize];
+        for (int i = 0; i < storageSize; i++) {
+            resumeWithoutNulls[i] = storage[i];
         }
         return resumeWithoutNulls;
     }
 
     int size() {
-        int size = 0;
-        for (Resume resume : storage) {
-            if (resume != null) {
-                size++;
-            }
-        }
-        return size;
+        return storageSize;
     }
 }
